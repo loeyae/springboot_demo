bdfd97e5044a09ffe65779fd04e8876fb93bd908
zhangyi <zhangyi07@beyondsoft.com>
2019-11-11 14:45:10 +0800

    update build.groovy

--- src/main/ci/build.groovy
+++ src/main/ci/build.groovy
@@ -1,17 +1,4 @@
 node {
-    stage("checkout") {
-        checkout(
-                [
-                        $class                           : 'GitSCM',
-                        branches                         : [[name: '*/master']],
-                        browser                          : [$class: 'GithubWeb', repoUrl: 'https://github.com/loeyae/springboot_demo'],
-                        doGenerateSubmoduleConfigurations: false,
-                        extensions                       : [],
-                        submoduleCfg                     : [],
-                        userRemoteConfigs                : [[credentialsId: 'github', url: 'https://github.com/loeyae/springboot_demo']]
-                ]
-        )
-    }
     stage("unit test") {
         sh "mvn org.jacoco:jacoco-maven-plugin:prepare-agent -f pom.xml clean test -Dautoconfig" +
                 ".skip=true -Dmaven.test.skip=false -Dmaven.test.failure.ignore=true"
