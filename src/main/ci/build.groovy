24f040fec5e27132dc06a3cd55fe3d792f920b16
zhangyi <zhangyi07@beyondsoft.com>
2019-11-11 15:53:20 +0800

    增肌代码静态扫描

--- src/main/ci/build.groovy
+++ src/main/ci/build.groovy
@@ -1,8 +1,30 @@
 node {
+    stage("checkout") {
+        checkout(
+                [
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: '*/master']],
+                        browser                          : [$class: 'GithubWeb', repoUrl: 'https://github.com/loeyae/springboot_demo'],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions                       : [],
+                        submoduleCfg                     : [],
+                        userRemoteConfigs                : [[credentialsId: 'github', url: 'https://github.com/loeyae/springboot_demo']]
+                ]
+        )
+    }
     stage("unit test") {
         sh "mvn org.jacoco:jacoco-maven-plugin:prepare-agent -f pom.xml clean test -Dautoconfig" +
                 ".skip=true -Dmaven.test.skip=false -Dmaven.test.failure.ignore=true"
         junit '**/target/surefire-reports/*.xml'
         jacoco buildOverBuild: true, changeBuildStatus: true, deltaInstructionCoverage: '60', maximumInstructionCoverage: '90', minimumInstructionCoverage: '70'
+    }
+    stage("code analysis") {
+        withSonarQubeEnv('Sonarqube') {
+            //固定使用项目根目录${basedir}下的pom.xml进行代码检查
+            sh "mvn -f pom.xml clean compile sonar:sonar "
+        }
+        timeout(1) {
+            waitForQualityGate abortPipeline: true
+        }
     }
 }
