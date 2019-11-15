2c3a796dc9b82f6bb46d95f6c4359661268dd2fa
ZhangYi <loeyae@gmail.com>
2019-11-15 19:00:31 +0800

    更新删除无用image

--- src/main/ci/build.groovy
+++ src/main/ci/build.groovy
@@ -81,8 +81,10 @@ node {
             }
             sh """
                 docker rmi $imageTag
-                docker rmi \$(docker images | grep "^<none>" | awk "{print \$3}")
                 """
+            sh '''
+                docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+                '''
         } else {
             echo "Task FAILURE, Skip image push"
         }
