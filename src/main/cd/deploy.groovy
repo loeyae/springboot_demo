cc9c50796a5795bcec843f5abb530e0efd59452c
ZhangYi <loeyae@gmail.com>
2019-11-23 14:19:13 +0800

    checkoud deploy脚本

--- src/main/cd/deploy.groovy
+++ src/main/cd/deploy.groovy
@@ -5,6 +5,19 @@ node {
                             false)
             ])
     ])
+    stage("Checkout") {
+        checkout(
+                [
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: '*/master']],
+                        browser                          : [$class: 'GithubWeb', repoUrl: 'https://github.com/loeyae/springboot_demo'],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions                       : [],
+                        submoduleCfg                     : [],
+                        userRemoteConfigs                : [[credentialsId: 'github', url: 'git@github.com:loeyae/springboot_demo.git']]
+                ]
+        )
+    }
     stage("deploy") {
         sh "kubectl apply -f src/main/cd/deploy-${params.ENV_LABEL}.yml"
     }
