04669f600842e6dab1516ddffc4dde340fc1818c
ZhangYi <loeyae@gmail.com>
2019-11-23 13:34:58 +0800

    增加deploy配置

---
+++ src/main/cd/deploy.groovy
@@ -0,0 +1,11 @@
+node("deploy") {
+    properties([
+            parameters([
+                    string(defaultValue: 'dev', description: '部署环境标志', name: 'ENV_LABEL', trim:
+                            false)
+            ])
+    ])
+    stage("deploy") {
+        sh "kubectl apply -f src/main/cd/deploy-${params.ENV_LABEL}.yml"
+    }
+}
